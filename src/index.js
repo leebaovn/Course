f46cdd24e87a3df85f7593ffab7b2a91309437b2
Lee Bao <leebao101@gmail.com>
2020-08-19 22:08:19 +0700

    add ext

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 const path = require('path');
 
 const route = require('./routes');
@@ -8,23 +8,25 @@ const route = require('./routes');
 const app = express();
 
 //Static files
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //template engine
-app.engine('hbs', handlebars({
-  extname:'hbs'
-}));
+app.engine(
+  'hbs',
+  handlebars({
+    extname: 'hbs',
+  }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //HTTP Logging
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 //Routes init
 route(app);
-
 
 app.listen(3003);
